bc560666da3f6426ea6ae572128dce0071f8da55
Maliseni1 <maliseni1205@gmail.com>
2026-01-16 13:18:53 +0200

    Updated the passport.js

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback" // Relative path works if proxy/host is set correctly
+    callbackURL: "https://citywatch-trco.onrender.com/api/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
